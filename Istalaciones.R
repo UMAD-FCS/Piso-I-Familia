d941dbd097d136498d782458fb3493ab69cf7997
UMAD-FCS <64287878+UMAD-FCS@users.noreply.github.com>
2022-05-25 08:49:35 -0300

    Add files via upload

---
+++ Istalaciones.R
@@ -0,0 +1,13 @@
+
+
+
+
+install.packages("tidyverse")
+install.packages("plotly")
+install.packages("ggplot2")
+install.packages("DT")
+install.packages("kableExtra")
+
+base        <- readxl::read_excel("Data/base_motor_Empleo.xls")
+base_fichas <- readxl::read_excel("Data/Base_Fichas_Técnicas.xls")
+
---
+++ Renderizar.R
@@ -0,0 +1,2 @@
+library(rmarkdown)
+render("C:/Users/Shari/Dropbox/UMAD/Sociodemografico/Piso-I_Familia/Piso-I---Familia/Piso_I_Familia.Rmd")
